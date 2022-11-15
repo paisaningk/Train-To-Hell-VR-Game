0d9f4b5e4b49de40094fd4b823af86e711231f48
paisaningk <gaw.fd@hotmail.com>
2022-11-15 18:42:09 +0700

    first commit.

---
+++ Assets/Script/WayPoint/WayPoint.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Script.WayPoint
+{
+    public class WayPoint : MonoBehaviour
+    {
+        public GameObject itemToMove;
+        public GameObject[] wayPointGameObjects;
+
+        public void AddWayPoint()
+        {
+            
+        }
+    }
+}
