13022cdd024c2c2c10ff4f0a3e486cb51254928d
paisaningk <gaw.fd@hotmail.com>
2022-11-22 14:54:41 +0700

    update gun and enemy , add new polygon

---
+++ Assets/Script/Enemy/EnemyData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+namespace Script.Enemy
+{
+    public class EnemyData : MonoBehaviour
+    {
+        public int hp = 100;
+        public Transform spawnPoint;
+        public TMP_Text damageText;
+
+
+        public void DoDamage()
+        {
+            var range = Random.Range(50, 100);
+            hp -= range;
+            var instantiate = Instantiate(damageText, spawnPoint);
+            instantiate.text = $"{range}";
+            StartCoroutine(ShowTextAMoment(instantiate));
+        }
+        
+        IEnumerator ShowTextAMoment(TMP_Text text)
+        {
+            text.gameObject.transform.DOScale((Vector3.one * 2), 2f);
+            yield return new WaitForSeconds(3);
+            DestroyImmediate(text.gameObject);
+        }
+    }
+}
---
+++ Assets/Script/Gun/GunController.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using Script.Enemy;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+
+namespace Script
+{
+    public class GunController : MonoBehaviour
+    {
+        private XRIDefaultInputActions vrInputActions;
+        [SerializeField] private GunComponent leftGunComponents;
+        [SerializeField] private GunComponent rightGunComponents;
+        [SerializeField] private LayerMask shootLayerMask;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            vrInputActions = new XRIDefaultInputActions();
+            vrInputActions.Enable();
+            vrInputActions.XRILeftHandInteraction.Activate.started += context => Shoot(leftGunComponents);
+            vrInputActions.XRIRightHandInteraction.Activate.started += context => Shoot(rightGunComponents);
+        }
+
+        private void OnValidate()
+        {
+            DrawLineBothHand();
+        }
+
+        private void Update()
+        {
+            DrawLineBothHand();
+        }
+
+        private void Shoot(GunComponent gunComponent)
+        {
+            Debug.Log("Shoot");
+            
+            gunComponent.shootSource.Play();
+            
+            Ray ray = new Ray(gunComponent.startPositionLine.position,gunComponent.startPositionLine.forward);
+                
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, 5, shootLayerMask))
+            {
+                if (raycastHit.collider.TryGetComponent(out EnemyData enemyData ))
+                {
+                    enemyData.DoDamage();
+                    Debug.Log(enemyData.name);
+                }
+            }
+        }
+
+        private void DrawLineBothHand()
+        {
+            DrawLine(leftGunComponents);
+            DrawLine(rightGunComponents);
+        }
+
+        private void DrawLine(GunComponent gunComponent)
+        {
+            var startPosition = gunComponent.gunLineRenderer.transform.position;
+            gunComponent.gunLineRenderer.SetPosition(0, startPosition);
+            gunComponent.gunLineRenderer.SetPosition(1, startPosition + (gunComponent.startPositionLine.forward * 5));
+        }
+    }
+
+    [Serializable]
+    public struct GunComponent
+    {
+        public LineRenderer gunLineRenderer;
+        public Transform startPositionLine;
+        public AudioSource shootSource;
+    }
+}
