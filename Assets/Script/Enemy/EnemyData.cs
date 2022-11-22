cfabdb11ab09d52d7b584958fa22b990b5e048d7
paisaningk <gaw.fd@hotmail.com>
2022-11-23 02:52:49 +0700

    enemy data

--- Assets/Script/Enemy/EnemyData.cs
+++ Assets/Script/Enemy/EnemyData.cs
@@ -1,30 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using DG.Tweening;
+using Sirenix.OdinInspector;
 using TMPro;
+using UnityEditor.Animations;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Script.Enemy
 {
     public class EnemyData : MonoBehaviour
     {
-        public int hp = 100;
+        public int hp = 3;
         public Transform spawnPoint;
         public TMP_Text damageText;
+        public Animator animator;
+        public bool isDead = false;
 
+        public void Start()
+        {
+            RandomAnimation();
+        }
+
+        [Button]
+        private void RandomAnimation()
+        {
+            var random = Random.Range(1, 4);
+            Debug.Log(random);
+            animator.SetInteger("A",random);
+        }
 
         public void DoDamage()
         {
             var range = Random.Range(50, 100);
-            hp -= range;
+            hp -= 1;
+            
             var instantiate = Instantiate(damageText, spawnPoint);
             instantiate.text = $"{range}";
+            
             StartCoroutine(ShowTextAMoment(instantiate));
+            
+            if (hp <= 0 && !isDead)
+            {
+                isDead = true;
+                Dead();
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void Dead()
+        {
+            
         }
         
         IEnumerator ShowTextAMoment(TMP_Text text)
         {
             text.gameObject.transform.DOScale((Vector3.one * 2), 2f);
-            yield return new WaitForSeconds(3);
+            yield return new WaitForSeconds(2f);
             DestroyImmediate(text.gameObject);
         }
     }
---
+++ Assets/Script/Sound/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Script.Singleton;
+using UnityEngine;
+
+namespace Script.Sound
+{
+    public class SoundManager : Singleton<SoundManager>
+    {
+        public List<SoundData> soundDataSet;
+        public AudioSource audioSource;
+
+        public void Play(string id)
+        {
+            var soundData = soundDataSet.FirstOrDefault(data => data.id == id);
+
+            audioSource.clip = soundData.clip;
+            audioSource.Play();
+        }
+    }
+
+    [Serializable]
+    public class SoundData
+    {
+        public string id;
+        public AudioClip clip;
+    }
+}
